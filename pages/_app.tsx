896f163c357a67a0429f9b4e79697ba35314e43d
Sami <haminesami@gmail.com>
2021-03-22 22:20:02 +0100

    Add home file

---
+++ pages/_app.tsx
@@ -0,0 +1,108 @@
+import App from 'next/app'
+import React from 'react'
+import { NextGithubMediaStore } from 'next-tinacms-github'
+import { TinaCMS, TinaProvider,  } from 'tinacms'
+import {
+  GithubClient,
+  TinacmsGithubProvider,
+  GithubMediaStore,
+} from 'react-tinacms-github'
+
+
+const github = new GithubClient({
+  proxy: '/api/proxy-github',
+  authCallbackRoute: '/api/create-github-access-token',
+  clientId: process.env.GITHUB_CLIENT_ID, //process.env.GITHUB_CLIENT_ID,
+  baseRepoFullName: process.env.REPO_FULL_NAME, //process.env.REPO_FULL_NAME, // e.g: tinacms/tinacms.org,
+  baseBranch: process.env.BASE_BRANCH, // e.g. 'master' or 'main' on newer repos
+  authScope: 'repo' // normally defaults to 'public_repo'
+})
+
+const MainLayout = ({ Component, pageProps }) => {
+  const tinaConfig = {
+    enabled: pageProps.preview,
+    toolbar: pageProps.preview,
+    sidebar: pageProps.preview,
+    apis: {
+      github
+    },
+    media: new NextGithubMediaStore(github),
+  }
+  const cms = React.useMemo(() => new TinaCMS(tinaConfig), [])
+
+  const enterEditMode = async () => {
+    const token = localStorage.getItem('tinacms-github-token') || null
+    const headers = new Headers()
+
+    if (token) {
+      headers.append('Authorization', 'Bearer ' + token)
+    }
+
+    const response = await fetch(`/api/preview`, { headers })
+    const data = await response.json()
+
+    if (response.status === 200) {
+      window.location.reload()
+    } else {
+      throw new Error(data.message)
+    }
+  }
+
+  const exitEditMode = () => {
+    fetch(`/api/reset-preview`).then(() => {
+      window.location.reload()
+    })
+  }
+
+  return (
+    <TinaProvider cms={cms} styled={false}>
+        <TinacmsGithubProvider
+          onLogin={enterEditMode}
+          onLogout={exitEditMode}
+          error={pageProps.error}
+        >
+          <EditLink cms={cms} />
+          <Component {...pageProps} />
+        </TinacmsGithubProvider>
+    </TinaProvider>
+  )
+}
+
+function useShouldLoadFont(cms: TinaCMS) {
+  const [enabled, setEnabled] = React.useState(cms.enabled)
+
+  React.useEffect(() => {
+    if (cms.enabled) return
+    return cms.events.subscribe('cms:enable', () => {
+      setEnabled(true)
+    })
+  }, [])
+
+  return enabled
+}
+
+class Site extends App {
+  componentDidMount() {
+    if (process.env.NODE_ENV === 'production') {
+    }
+  }
+
+  render() {
+    const { Component, pageProps } = this.props
+    return <MainLayout Component={Component} pageProps={pageProps} />
+  }
+}
+
+export interface EditLinkProps {
+  cms: TinaCMS
+}
+
+export const EditLink = ({ cms }: EditLinkProps) => {
+  return (
+    <button onClick={() => cms.toggle()}>
+      {cms.enabled ? 'Exit Edit Mode' : 'Edit This Site'}
+    </button>
+  )
+}
+
+export default Site
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+import { getGithubPreviewProps, parseJson } from 'next-tinacms-github'
+import { GetStaticProps } from 'next'
+
+
+export default function Home({ file }) {
+  const data = file.data
   return (
     <div className={styles.container}>
       <Head>
@@ -11,7 +16,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          {data.title}
         </h1>
 
         <p className={styles.description}>
@@ -63,3 +68,28 @@ export default function Home() {
     </div>
   )
 }
+
+
+export const getStaticProps: GetStaticProps = async function({
+  preview,
+  previewData,
+ }) {
+  if (preview) {
+    return getGithubPreviewProps({
+      ...previewData,
+      fileRelativePath: 'content/home.json',
+      parse: parseJson,
+    })
+  }
+  return {
+    props: {
+      sourceProvider: null,
+      error: null,
+      preview: false,
+      file: {
+        fileRelativePath: 'content/home.json',
+        data: (await import('../content/home.json')).default,
+      },
+    },
+  }
+ }
